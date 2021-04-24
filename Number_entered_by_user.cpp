689f6385d7591a9951d4430d0f75b49326e27ee7
Harsha Desu <harshadesu@gmail.com>
2021-04-24 19:58:28 +0530

    Create Number_entered_by_user.cpp

---
+++ Number_entered_by_user.cpp
@@ -0,0 +1,14 @@
+/* Print the number entered by the user*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter a number:";
+    cin >> number;                       // cin is used to scan the entered number
+    cout << "Your number is:" << number; // cout is used to print the number
+    return 0;
+}
+
+/* Output: Enter a number : 5
+ Your number is:5 */
